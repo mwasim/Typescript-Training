9fed9d10d64ef85195cba5eb44de1209736dd4c2
Wasim <mwasimse@gmail.com>
2024-01-11 19:11:18 +0500

    TypescriptEssentialTraining: IndexAccessTypes demo

---
+++ typescript-essential-training/src/ComplexTypes/IndexAccessTypes.ts
@@ -0,0 +1,35 @@
+type TheContactStatus = "active" | "inactive" | "new";
+
+interface TheAddress {
+  street: string;
+  province: string;
+  postalCode: string;
+}
+
+interface TheContact {
+  id: number;
+  name: string;
+  status: TheContactStatus;
+  address: TheAddress;
+}
+
+interface TheContactEvent {
+  contactId: number;
+}
+
+interface TheContactDeletedEvent extends TheContactEvent {}
+
+interface TheContactStatusChangedEvent extends TheContactEvent {
+  oldStatus: TheContactStatus;
+  newStatus: TheContactStatus;
+}
+
+interface TheContactEvents {
+  deleted: TheContactDeletedEvent;
+  statusChanged: TheContactStatusChangedEvent;
+  // ... and so on
+}
+
+function getValue<T, U extends keyof T>(source: T, propertyName: U) {
+  return source[propertyName];
+}
