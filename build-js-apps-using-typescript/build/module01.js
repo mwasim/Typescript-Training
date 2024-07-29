8ec2f2297ce7ebdb41a09804e95ad09d0ebe5d29
Wasim <mwasimse@gmail.com>
2024-07-29 21:00:22 +0500

    Build JavaScript Apps using TypeScript - Module01 completed.

---
+++ build-js-apps-using-typescript/build/module01.js
@@ -0,0 +1,5 @@
+"use strict";
+const addNumbers = (num1, num2) => {
+    return num1 + num2;
+};
+console.log(addNumbers(3, 6));
---
+++ build-js-apps-using-typescript/module01.ts
@@ -0,0 +1,5 @@
+const addNumbers = (num1: number, num2: number) => {
+  return num1 + num2;
+};
+
+console.log(addNumbers(3, 6));
