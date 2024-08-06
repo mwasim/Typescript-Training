9e2f7fc07410a680d696ee2f475cee0d908bb07a
Wasim <mwasimse@gmail.com>
2024-08-06 17:00:59 +0500

    Build JavaScript Apps using TypeScript - Module-07: Accessing third party libraries - dotenv library demo to read variables from .env file.

---
+++ build-js-apps-using-typescript/code/module-07/m07-start/main.ts
@@ -0,0 +1,34 @@
+/*
+    Importing libraries
+    - In TypeScript, you gain access to them by using the import statement.
+
+    Type libraries
+    - he open-source project DefinitelyTyped is a repository of TypeScript type definitions for many popular JavaScript libraries. 
+
+    Go to DefinitelyTyped and search for the dotenv type library. DefinitelyTyped will provide the name of the library to install and other implementation details.
+    1). npm install dotenv // install the dotenv type library
+    2). npm install --save-dev @types/node @types/dotenv //node provides access to process.env so you can access it from your code.
+
+    //Create a new file in the root directory of your project called .env. This file will contain environment-specific variables for the project.
+    //Below, demo code example shows how to check for errors and use variables from .env file.
+*/
+
+import dotenv from "dotenv";
+
+/*
+    config reads your .env file, parses the contents, 
+    assigns it to process.env, and returns an object with a 
+    parsed key containing the loaded content or an error key if it failed.
+*/
+const result = dotenv.config();
+
+if (result.error) {
+  throw result.error;
+}
+
+//Return the contents of the parsed property to the console.
+console.log(result.parsed); // Returns { DB_HOST: 'localhost', WEB_HOST: 'staging.adventure-works.com' }
+
+//Access the values contains in each key in process.env and return the value to the console.
+console.log(process.env.DB_HOST);
+console.log(process.env.WEB_HOST);
